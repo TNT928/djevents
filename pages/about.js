aad7e0db3f777e4cea25cb8e793bf710ebe5cc8b
Michael Vanderloon <jenkins92886@gmail.com>
2021-04-30 10:39:56 -0400

    Layout, Header and footer

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,12 @@
+import Layout from '../components/Layout'
+
 export default function AboutPage() {
     return (
-        <div>
+        <Layout title='About DJ Events'>
             <h1>About</h1>
             <p>This is an app to find the latest DJ and other muscial events</p>
             <p>Verison:1.0.0</p>
-        </div>
+
+        </Layout>
     )
 }

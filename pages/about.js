9dc4f9e74b17072b7b3e04b8198bb3a9eb6c3a28
Michael Vanderloon <jenkins92886@gmail.com>
2021-04-30 09:33:03 -0400

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the latest DJ and other muscial events</p>
+            <p>Verison:1.0.0</p>
+        </div>
+    )
+}
